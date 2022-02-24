ed2b2a01228551bcbd706a997f8baf4fbd235eed
vgpro54321 <vladimir.gaytanov.pro@gmail.com>
2022-02-23 17:37:51 -0800

    1 file modified

--- src/app/transform-helper.ts
+++ src/app/transform-helper.ts
@@ -4,5 +4,31 @@ export class TransformHelper {
     angle: number
   ): void {
     const angleNormalized = TransformHelper.normalizeAngle(angle);
+
+    switch (angle) {
+      case 90: {
+        break;
+      }
+
+      case 180: {
+        break;
+      }
+
+      case 270: {
+        break;
+      }
+
+      default:
+        throw new Error('Angle must be a multiple of 90 degrees.');
+    }
+  }
+
+  static normalizeAngle(angle: number): number {
+    let rem = angle % 360;
+    if (rem < 0) {
+      rem += 360;
+    }
+
+    return rem;
   }
 }
